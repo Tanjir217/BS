088a8b9b593b3e8b87ab446168d8f8ae2dd6f231
tanjir <shawalmh46@gmail.com>
2026-08-25 16:10:59 +0600

    the fucking bug

--- src/components/Header/navBar/Logo.jsx
+++ src/components/Header/navBar/Logo.jsx
@@ -1,26 +1,14 @@
 import React from 'react'
-import {Link}  from 'react-router-dom'
+import { Link } from 'react-router-dom'
+
 function Logo() {
     return (
-        <div
-        
+        <Link
+        to="/"
+        className="text-4xl font-semibold font-mono tracking-wider no-underline text-black hover:text-black hover:no-underline"
         >
-            <Link
-            
-            to={'/'}
-            className='no-underline'
-            >
-            <h1
-            className='text-4xl no-underline font-semibold font-mono tracking-wider'
-            >
-                BAYZID SHOES
-            </h1>
-            </Link>
-            
-            
-            
-            
-        </div>
+        BAYZID SHOES
+        </Link>
     )
 }
 
