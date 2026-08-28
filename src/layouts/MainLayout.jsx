cd89ab19e77ff291c7497f479874b2259ca5e01d
tanjir <shawalmh46@gmail.com>
2026-08-29 02:48:58 +0600

    what the hack i had done still can't figure it out

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -13,6 +13,7 @@ function MainLayout() {
             <Outlet/>
         </main>
         <Footer/>
+        
         </>
     )
 }
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layouts/MainLayout'
 import Home from '../pages/home/Home'
+import NotFound from '../pages/home/NotFound'
 const router = createBrowserRouter([
     {
         path: "/",
         element: <MainLayout/>,
+        errorElement: <NotFound/>,
         children: [{
             index: true,
             element: <Home/>
         }]
+
     }
 ])
     
