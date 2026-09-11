9fa8df915947ac2066d435d193006292a54207b8
tanjir <shawalmh46@gmail.com>
2026-09-11 19:15:40 +0600

    fixing the mega menu again

--- src/utils/navigationTree.js
+++ src/utils/navigationTree.js
@@ -125,7 +125,7 @@ import {
     if (leafChildren.length > 0) {
       columns.push({
         id: `${category.$id}-shop`,
-        title: "SHOP",
+        title: "",
         href: getCategoryUrl(category),
   
         links: leafChildren.map(
